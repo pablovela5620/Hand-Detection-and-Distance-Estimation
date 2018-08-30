0a14da8aacfaa1e39095e3f8e26857d7655d6484
pablovela5620 <pablovela5620@gmail.com>
2018-08-30 11:48:34 -0400

    Added option to not visualize in order to increase fps

--- hand_detection.py
+++ hand_detection.py
@@ -1,59 +1,67 @@
 from utils import detector_utils as detector_utils
 import cv2
-import tensorflow as tf
 import datetime
-from imutils.video import VideoStream, FPS
+import imutils
+from imutils.video import VideoStream
 
 detection_graph, sess = detector_utils.load_inference_graph()
 
-if __name__ == '__main__':
+VIZ_FLAG = False
 
+if __name__ == '__main__':
     # Detection confidence threshold to draw bounding box
     score_thresh = 0.60
 
     # Get stream from webcam and set parameters)
     vs = VideoStream().start()
 
+    # max number of hands we want to detect/track
+    num_hands_detect = 1
+
     # Used to calculate fps
     start_time = datetime.datetime.now()
     num_frames = 0
-    im_fps = FPS().start()
 
-    im_height, im_width = (vs.read().shape[0], vs.read().shape[1])
-    # max number of hands we want to detect/track
-    num_hands_detect = 2
+    im_height, im_width = (None, None)
 
-    while True:
-        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        image_np = vs.read()
+    try:
+        while True:
+            # Read Frame and process
+            frame = vs.read()
+            frame = cv2.resize(frame, (200, 200))
 
-        # Convert image to rgb since opencv loads images in bgr, if not accuracy will decrease
-        try:
-            image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-        except:
-            print("Error converting to RGB")
+            if im_height == None:
+                im_height, im_width = frame.shape[:2]
+            # Convert image to rgb since opencv loads images in bgr, if not accuracy will decrease
+            try:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            except:
+                print("Error converting to RGB")
 
-        # Run image through tensorflow graph
-        boxes, scores, classes = detector_utils.detect_objects(
-            image_np, detection_graph, sess)
+            # Run image through tensorflow graph
+            boxes, scores, classes = detector_utils.detect_objects(
+                frame, detection_graph, sess)
 
-        # Draw bounding boxeses and text
-        detector_utils.draw_box_on_image(
-            num_hands_detect, score_thresh, scores, boxes, classes, im_width, im_height, image_np)
+            # Draw bounding boxeses and text
+            detector_utils.draw_box_on_image(
+                num_hands_detect, score_thresh, scores, boxes, classes, im_width, im_height, frame)
 
-        # Calculate Frames per second (FPS)
-        num_frames += 1
-        elapsed_time = (datetime.datetime.now() -
-                        start_time).total_seconds()
-        fps = num_frames / elapsed_time
-        im_fps.update()
+            # Calculate Frames per second (FPS)
+            num_frames += 1
+            elapsed_time = (datetime.datetime.now() -
+                            start_time).total_seconds()
+            fps = num_frames / elapsed_time
 
-        # Display FPS on frame
-        detector_utils.draw_text_on_image("FPS : " + str("{0:.2f}".format(fps)), image_np)
-        cv2.imshow('Detection', cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            vs.stop()
-            break
-    im_fps.stop()
-    print("Average FPS: ", im_fps.fps())
+            if VIZ_FLAG:
+                # Display FPS on frame
+                detector_utils.draw_text_on_image("FPS : " + str("{0:.2f}".format(fps)), frame)
+                cv2.imshow('Detection', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    cv2.destroyAllWindows()
+                    vs.stop()
+                    break
+
+        print("Average FPS: ", str("{0:.2f}".format(fps)))
+
+    except KeyboardInterrupt:
+        print("Average FPS: ", str("{0:.2f}".format(fps)))
